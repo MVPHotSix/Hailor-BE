1ca4e164668d25f7c397cd8be2f6f6340f34cb3f
konempty <jameswebb@naver.com>
2025-02-10 21:09:25 +0900

    project init

---
+++ src/main/java/kr/hailor/hailor/HailorApplication.java
@@ -0,0 +1,13 @@
+package kr.hailor.hailor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HailorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HailorApplication.class, args);
+    }
+
+}
