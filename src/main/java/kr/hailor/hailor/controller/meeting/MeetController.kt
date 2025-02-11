f88d84e7c48ba9d1f24e8e2ef3b938158e6162cf
konempty <jameswebb@naver.com>
2025-02-12 01:19:40 +0900

    구글 밋 연동 추가

---
+++ src/main/java/kr/hailor/hailor/controller/meeting/MeetController.kt
@@ -0,0 +1,14 @@
+package kr.hailor.hailor.controller.meeting
+
+import kr.hailor.hailor.service.GoogleCalendarService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MeetController(
+    private val calendarService: GoogleCalendarService,
+) {
+    // TODO 지금은 확인을 위해 임시로 만들어두었지만 나중에는 예약 확정시 이 링크를 생성해줘야 한다
+    @GetMapping("/create-meet")
+    fun createMeetLink(): String = calendarService.createGoogleMeetLink()
+}
---
+++ src/main/java/kr/hailor/hailor/util/GoogleAuthorizeUtil.kt
@@ -0,0 +1,50 @@
+package kr.hailor.hailor.util
+
+import com.google.api.client.auth.oauth2.Credential
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.json.JsonFactory
+import com.google.api.client.json.gson.GsonFactory
+import com.google.api.client.util.store.FileDataStoreFactory
+import kr.hailor.hailor.config.properties.HostProperties
+import org.springframework.core.io.ClassPathResource
+import org.springframework.stereotype.Component
+import java.io.File
+import java.io.InputStreamReader
+import java.util.Collections
+
+@Component
+class GoogleAuthorizeUtil(
+    private val hostProperties: HostProperties,
+) {
+    companion object {
+        private val JSON_FACTORY: JsonFactory = GsonFactory.getDefaultInstance()
+        private const val TOKENS_DIRECTORY_PATH = "tokens"
+    }
+
+    fun authorize(): Credential {
+        val inputStream = ClassPathResource("credentials.json").inputStream
+        val clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, InputStreamReader(inputStream))
+
+        val flow =
+            GoogleAuthorizationCodeFlow
+                .Builder(
+                    GoogleNetHttpTransport.newTrustedTransport(),
+                    JSON_FACTORY,
+                    clientSecrets,
+                    Collections.singletonList("https://www.googleapis.com/auth/calendar"),
+                ).setDataStoreFactory(FileDataStoreFactory(File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build()
+        val receiver =
+            LocalServerReceiver
+                .Builder()
+                .setHost(hostProperties.apiServer)
+                .setPort(443)
+                .build()
+        return AuthorizationCodeInstalledApp(flow, receiver).authorize("user") // TODO user 부분은 사용자 유니크키로 잡아야 할듯
+    }
+}
