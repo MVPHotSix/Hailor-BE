dc63e2136fe7464b41a546c59ff85d60339bdb66
hyeon51237 <hyeon51237@naver.com>
2025-02-14 19:35:02 +0900

    .

---
+++ src/main/java/kr/hailor/hailor/config/MailConfig.java
@@ -0,0 +1,27 @@
+package kr.hailor.hailor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("hyeon51237@gmail.com");
+        mailSender.setPassword("${EMAIL_PASSWORD}");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+}
