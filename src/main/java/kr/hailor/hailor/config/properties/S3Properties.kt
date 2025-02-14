e6983d983ed5dcf73f437a36d3bb61d5378ae3f7
konempty <jameswebb@naver.com>
2025-02-14 10:08:36 +0900

    S3 연동 추가

---
+++ src/main/java/kr/hailor/hailor/config/properties/S3Properties.kt
@@ -0,0 +1,8 @@
+package kr.hailor.hailor.config.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "spring.cloud.aws.s3")
+data class S3Properties(
+    val bucket: String,
+)
